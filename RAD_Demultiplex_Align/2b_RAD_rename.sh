16e72a2a263b7e94759491f953a86635eb66a06c
isilverg <100424652+isilverg@users.noreply.github.com>
2023-02-15 12:39:33 -0500

    Add files via upload

---
+++ RAD_Demultiplex_Align/2b_RAD_rename.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+
+list=$1
+#enter metadata file as argument 1 in command line (cc_keyfile.txt)
+x=1
+while [ $x -le 96 ]
+do
+
+	string="sed -n ${x}p ${list}"
+	str=$($string)
+
+	var=$(echo $str | awk -F"\t" '{print $1, $2, $3, $4}')
+	set -- $var
+	c1=$1 #well barcode
+	c2=$2 #RAD well ID
+	c3=$3 #SampleID
+	c4=$4 #Location
+
+#run with just RA and RB files in different subfolders; run for RA, then edit accordingly and run for RB
+#run with echo to check is correct, then if all looks good remove and run without.
+mv ./CC_RB/RAD-CCGOM_RB_GG${c1}TGCAGG.fastq ../2_demultiplexed_renamed_fastq/RAD-CCGOM_${c2}_${c3}_${c4}_RB.fastq
+#RAD-CCGOM_RA_GGAACCGAGATGCAGG.fastq wA01_MML257_NGM_RA.fastq
+	x=$(( $x + 1 ))
+
+done
