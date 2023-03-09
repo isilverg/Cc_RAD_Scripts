073073c207c94873a9c0c75a25982bc5d712d3d8
isilverg <100424652+isilverg@users.noreply.github.com>
2023-03-09 14:13:54 -0500

    Script for admixture

---
+++ PopGen/admixture.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH -n 4
+#SBATCH -p eoas_q
+#SBATCH -t 4:00:00
+#SBATCH -e admixture.err
+
+pops=10
+
+for K in $( seq 0 $pops )
+
+do
+
+./admixture --cv ccgom.bed $K | tee log${K}.out
+
+done
