22156251b3d5c2f0a81b0b357ba3b33d62251801
isilverg <100424652+isilverg@users.noreply.github.com>
2025-07-29 08:50:16 -0400

    Update 3_vcf_filter.sh

--- SNP Filtering/3_vcf_filter.sh
+++ SNP Filtering/3_vcf_filter.sh
@@ -9,4 +9,4 @@ module load anaconda/3.8.3
 eval "$(conda shell.bash hook)"
 conda activate vcflib
 
-vcffilter -f "QUAL > 20" CCGOM_n7.vcf  | vcfallelicprimitives >SNP_only_CCGOM_n7.vcf
+vcffilter -f "QUAL > 20" COMBINED_n7.vcf  | vcfallelicprimitives >SNP_only_COMBINED_n7.vcf
